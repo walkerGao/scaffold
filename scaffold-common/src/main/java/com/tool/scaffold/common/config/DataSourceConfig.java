7b5a0c209e5fd3499d27ac3bee0147e1053d4c6a
gaowei <>
2022-01-07 17:58:06 +0800

    更改数据库连接池druid为hikaricp

--- scaffold-common/src/main/java/com/tool/scaffold/common/config/DataSourceConfig.java
+++ scaffold-common/src/main/java/com/tool/scaffold/common/config/DataSourceConfig.java
@@ -1,26 +1,20 @@
 package com.tool.scaffold.common.config;
 
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
-import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.aop.support.DefaultPointcutAdvisor;
-import org.springframework.aop.support.JdkRegexpMethodPointcut;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,7 +29,7 @@ public class DataSourceConfig {
     @Bean(name = "defaultDataSource")
     @Primary
     public DataSource defaultDataSource() {
-        return new DruidDataSource();
+        return new HikariDataSource();
     }
 
     @Bean(name = "dynamicDataSource")
@@ -48,10 +42,10 @@ public class DataSourceConfig {
         return dataSource;
     }
 
-    @ConfigurationProperties(prefix = "mybatis")
     @Bean(name = "sqlSessionFactoryBean")
-    public SqlSessionFactoryBean defaultSqlSessionFactoryBean(@Qualifier("dynamicDataSource") DataSource datasource) {
+    public SqlSessionFactoryBean defaultSqlSessionFactoryBean(@Qualifier("dynamicDataSource") DataSource datasource) throws IOException {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*.xml"));
         bean.setDataSource(datasource);
         return bean;
     }
@@ -66,59 +60,4 @@ public class DataSourceConfig {
     public SqlSessionTemplate defaultSqlSessionTemplate(@Qualifier("sqlSessionFactory") SqlSessionFactory sessionfactory) {
         return new SqlSessionTemplate(sessionfactory);
     }
-
-    /**
-     * druidServlet注册
-     */
-    @Bean
-    public ServletRegistrationBean druidServletRegistration() {
-        ServletRegistrationBean srb = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
-        //添加IP白名单
-        srb.addInitParameter("allow", "127.0.0.1");
-        // 添加控制台管理用户
-        srb.addInitParameter("loginUsername", "MarketingCenter");
-        srb.addInitParameter("loginPassword", "MC1234!@#$");
-        // 是否能够重置数据
-        srb.addInitParameter("resetEnable", "false");
-        return srb;
-    }
-
-    /**
-     * druid监控 配置URI拦截策略
-     */
-    @Bean
-    public FilterRegistrationBean druidStatFilter() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
-        //添加过滤规则.
-        filterRegistrationBean.addUrlPatterns("/*");
-        //添加不需要忽略的格式信息.
-        filterRegistrationBean.addInitParameter(
-                "exclusions", "/static/*,*?*,*.js,*.gif,*.jpg,*.png,*.css,*.txt,*.ico,/druid,/druid/*");
-        return filterRegistrationBean;
-    }
-
-    /**
-     * druid数据库连接池监控
-     */
-    @Bean
-    public DruidStatInterceptor druidStatInterceptor() {
-        return new DruidStatInterceptor();
-    }
-
-    @Bean
-    @Scope("prototype")
-    public JdkRegexpMethodPointcut druidStatPointcut() {
-        JdkRegexpMethodPointcut pointcut = new JdkRegexpMethodPointcut();
-        pointcut.setPatterns("com.tool.scaffold.dao.*", "com.tool.scaffold.manager.*", "com.tool.scaffold.service.*");
-        return pointcut;
-    }
-
-    @Bean
-    public DefaultPointcutAdvisor druidStatAdvisor(DruidStatInterceptor druidStatInterceptor, JdkRegexpMethodPointcut druidStatPointcut) {
-        DefaultPointcutAdvisor defaultPointAdvisor = new DefaultPointcutAdvisor();
-        defaultPointAdvisor.setPointcut(druidStatPointcut);
-        defaultPointAdvisor.setAdvice(druidStatInterceptor);
-        return defaultPointAdvisor;
-    }
-
 }
